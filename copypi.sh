ba20af03c006e026f0b4ee2ec6f2821e4ac62696
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-12-04 12:48:17 +0900

    cross compile

--- copypi.sh
+++ copypi.sh
@@ -3,8 +3,5 @@ if [ $# -lt 1 ]; then
     echo arg error: $*
     exit 1
 fi
-
+chmod 777 getters-linux-arm-5
 scp -p getters-linux-arm-5 pi@$1:/home/pi/priser/
-
- getters-linux-arm-7
- getters-linux-arm64
